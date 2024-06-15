c3ab0ed37d1ca7feb4958a7a13c8f2009b56bc1e
Harsh <hk755063@gmail.com>
2024-06-15 10:34:01 +0530

    resume updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,7 +11,7 @@ function Navbar(){
                 <Section sec="/Skills" text = "Skills" />
                 <Section sec="/Projects" text = "Projects" />
                 <Section sec="/contacts" text = "Contact" />
-                <a href= "https://drive.google.com/file/d/12OriX6OQLTtdhI0bSTPYfGY9ZsWweSkP/view?usp=sharing" target="_blank" rel="noreferrer" id="resume">Resume</a>
+                <a href= "https://drive.google.com/file/d/11_i9kfIATdGzpX76MASLsrHmsYcYD_AW/view" target="_blank" rel="noreferrer" id="resume">Resume</a>
             </nav>
         </div>
     );
