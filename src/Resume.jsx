6df58a82658ae9ae37d790e3811c364015d90eba
Harsh <hk755063@gmail.com>
2024-06-21 02:30:32 +0530

    resume updated

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -4,12 +4,12 @@ function Resume() {
   return (
     <div className="pdf-container">
       <iframe
-        src="https://drive.google.com/file/d/11_i9kfIATdGzpX76MASLsrHmsYcYD_AW/view"
+        src="https://drive.google.com/file/d/1cOyhX5GZn8DQkfJwaIncEohlr61_fz5n/view?usp=sharing"
         width="100%"
         height="600px"
       >
         This browser does not support PDFs. Please download the PDF to view it:
-        <a href="https://drive.google.com/file/d/11_i9kfIATdGzpX76MASLsrHmsYcYD_AW/view" target="_blank">Download PDF</a>.
+        <a href="https://drive.google.com/file/d/1cOyhX5GZn8DQkfJwaIncEohlr61_fz5n/view?usp=sharing" target="_blank">Download PDF</a>.
       </iframe>
     </div>
   );
